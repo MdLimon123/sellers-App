a7501432d77da857dc99a72baae40526be7f5f92
Limon Islam <limon222020@gmail.com>
2024-02-08 23:38:28 +0600

    register screen done

--- lib/authScreens/signup_screen.dart
+++ lib/authScreens/signup_screen.dart
@@ -2,6 +2,7 @@ import 'dart:io';
 
 import 'package:flutter/material.dart';
 import 'package:image_picker/image_picker.dart';
+import 'package:sellers_app/Widgets/custom_text_field.dart';
 
 class SignupScreen extends StatefulWidget {
   const SignupScreen({super.key});
@@ -11,6 +12,14 @@ class SignupScreen extends StatefulWidget {
 }
 
 class _SignupScreenState extends State<SignupScreen> {
+  final _formKey = GlobalKey<FormState>();
+
+  TextEditingController nameTextEditingController = TextEditingController();
+  TextEditingController emailTextEditingController = TextEditingController();
+  TextEditingController passwordTextEditingController = TextEditingController();
+  TextEditingController confirmPasswordTextEditingController = TextEditingController();
+  TextEditingController phoneTextEditingController = TextEditingController();
+  TextEditingController locationTextEditingController = TextEditingController();
 
   XFile? imageFile;
   ImagePicker pickedImage = ImagePicker();
@@ -44,7 +53,103 @@ class _SignupScreenState extends State<SignupScreen> {
               color: Colors.grey,)
               : null,
             ),
-          )
+          ),
+
+          const SizedBox(height: 10,),
+          Form(
+              key: _formKey,
+              child: Column(
+                children: [
+
+                  CustomTextField(
+                    textEditingController: nameTextEditingController,
+                    hintString: "Name",
+                    iconData: Icons.person,
+                    isObscure: false,
+                    enable: true,
+
+                  ),
+
+                  CustomTextField(
+                    textEditingController: emailTextEditingController,
+                    hintString: "Email",
+                    iconData: Icons.email,
+                    isObscure: false,
+                    enable: true,
+
+                  ),
+
+                  CustomTextField(
+                    textEditingController: phoneTextEditingController,
+                    hintString: "Phone",
+                    iconData: Icons.phone,
+                    isObscure: false,
+                    enable: true,
+
+                  ),
+
+                  CustomTextField(
+                    textEditingController: passwordTextEditingController,
+                    hintString: "Password",
+                    iconData: Icons.lock,
+                    isObscure: true,
+                    enable: true,
+
+                  ),
+                  CustomTextField(
+                    textEditingController: confirmPasswordTextEditingController,
+                    hintString: "Confirm Password",
+                    iconData: Icons.lock,
+                    isObscure: true,
+                    enable: true,
+
+                  ),
+
+                  CustomTextField(
+                    textEditingController: locationTextEditingController,
+                    hintString: "Cafe/Restaurant Address",
+                    iconData: Icons.my_location,
+                    isObscure: false,
+                    enable: true,
+
+                  ),
+                  const SizedBox(height: 10,),
+                  Container(
+                    height: 39,
+                    width: 398,
+                    alignment: Alignment.center,
+                    child: ElevatedButton.icon(
+                        onPressed: (){},
+                        style: ElevatedButton.styleFrom(
+                          backgroundColor: Colors.green,
+                          shape: RoundedRectangleBorder(
+                            borderRadius: BorderRadius.circular(32)
+                          )
+                        ),
+                        icon: const Icon(Icons.location_on,
+                        color: Colors.white,),
+                        label: const Text('Get My Current Location',
+                        style: TextStyle(
+                          color: Colors.white
+                        ),))
+                  ),
+                  const SizedBox(height: 20,),
+                  ElevatedButton(
+                      onPressed: (){},
+                      style: ElevatedButton.styleFrom(
+                          backgroundColor: Colors.blueAccent,
+                          padding: const EdgeInsets.symmetric(horizontal: 50, vertical: 10)
+                      ),
+                      child: const Text("Sign Up",
+                        style: TextStyle(
+                            color: Colors.white,
+                            fontWeight: FontWeight.bold
+                        ),
+                      )),
+                  const SizedBox(height: 20,),
+                ],
+              ))
+
 
         ],
       ),
