e39dc0a4351b605ada5ea77cee21dc4902381a41
Limon Islam <limon222020@gmail.com>
2024-02-08 23:17:43 +0600

    pick image

--- lib/authScreens/signup_screen.dart
+++ lib/authScreens/signup_screen.dart
@@ -1,4 +1,7 @@
+import 'dart:io';
+
 import 'package:flutter/material.dart';
+import 'package:image_picker/image_picker.dart';
 
 class SignupScreen extends StatefulWidget {
   const SignupScreen({super.key});
@@ -8,15 +11,41 @@ class SignupScreen extends StatefulWidget {
 }
 
 class _SignupScreenState extends State<SignupScreen> {
+
+  XFile? imageFile;
+  ImagePicker pickedImage = ImagePicker();
+
+  pickImageFormGallery()async{
+    imageFile = await pickedImage.pickImage(source: ImageSource.gallery);
+    setState(() {
+      imageFile;
+    });
+  }
+
+
   @override
   Widget build(BuildContext context) {
     return SingleChildScrollView(
       child: Column(
         children: [
-          Text(
-            'Signup',
-            style: TextStyle(fontSize: 30, color: Colors.grey),
+         const SizedBox(height: 11,),
+
+          InkWell(
+            onTap: (){
+              pickImageFormGallery();
+            },
+            child: CircleAvatar(
+              radius: MediaQuery.of(context).size.width * 0.20,
+              backgroundColor: Colors.white,
+              backgroundImage: imageFile == null ? null: FileImage(File(imageFile!.path)),
+              child: imageFile == null?
+              Icon(Icons.add_photo_alternate,
+              size: MediaQuery.of(context).size.width * 0.20,
+              color: Colors.grey,)
+              : null,
+            ),
           )
+
         ],
       ),
     );
